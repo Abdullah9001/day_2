66db9f9657fc8378b4e2d7eefab7d2602bbd87ae
Abdullah9001 <abdullahjubayer44@gmail.com>
2023-05-17 00:02:23 +0600

    second day first lession start

---
+++ app.js
@@ -0,0 +1,8 @@
+// javascript template litarals
+let country = "Bangladesh";
+let a = 17;
+let b = 1;
+
+let sentence = `I love ${country} an it's population is ${a + b} crore`;
+
+console.log(sentence);
